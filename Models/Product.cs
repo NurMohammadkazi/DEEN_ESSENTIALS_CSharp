f669772ee5e37d906bbc4f29faa89bc049718834
NurMohammadkazi <nurmohammadkazi2002@gmail.com>
2025-02-26 21:24:35 +0600

    Design of front end , admin page and login-up page and adding product to database

---
+++ Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Deen_Essentials.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product name is required")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal ProductPrice { get; set; }
+
+        [Required(ErrorMessage = "Category is required")]
+        public string ProductCategory { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        [Required(ErrorMessage = "Stock quantity is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock quantity must be 0 or greater")]
+        public int ProductStock { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Deen_Essentials.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Required(ErrorMessage = "Product name is required")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        public decimal ProductPrice { get; set; }
+
+        [Required(ErrorMessage = "Category is required")]
+        public string ProductCategory { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        [Required(ErrorMessage = "Stock quantity is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock quantity must be 0 or greater")]
+        public int ProductStock { get; set; }
+
+        [Required(ErrorMessage = "Product image is required")]
+        public IFormFile ProductImage { get; set; }
+    }
+}
